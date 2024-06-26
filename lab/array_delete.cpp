86ab7dc0871165604e9fa02a6ecd741c2d2e031b
Md Eibrahim Molla <122208401+mdeibrahim@users.noreply.github.com>
2024-06-26 22:57:18 +0600

    Add files via upload

---
+++ lab/array_delete.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+void print(int arr[])
+{
+    
+}
+int main()
+{
+     int arr[5]={1,2,3,4,5};
+     remove(arr,arr+5,3);
+     map<int,int>mp;
+     mp[5]=6;
+     cout<<mp.empty()<<endl;
+     
+   //for(auto x:arr)cout<<x<<" ";
+}
